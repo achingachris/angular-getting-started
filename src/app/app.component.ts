5cb842da556260eabc97429fbf7f28dd5120deca
ChrisAchinga <achinga.chris@gmail.com>
2020-07-09 14:55:43 +0300

    first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>
+      <h1>{{pageTitle}}</h1>
+      <div>My first component</div>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'Jamila Product Management'
 }
