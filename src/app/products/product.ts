bc991469bf7f025e11d4c5365e49cd52de91650b
ChrisAchinga <achinga.chris@gmail.com>
2020-07-10 18:42:03 +0300

    created interface

---
+++ src/app/products/product.ts
@@ -0,0 +1,11 @@
+// interface =====>
+export interface Iproduct {
+  productId: number;
+  productName: string;
+  productCode: string;
+  releaseDate: string;
+  price: number;
+  description: string;
+  starRating: number;
+  imageUrl: string;
+}
